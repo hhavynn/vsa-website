c93e2512fee2dce448f9f5b4f6d1aa2d24b60ba6
hhavynn <havynnguyen9@gmail.com>
2025-06-13 01:28:00 -0700

    feat: add descriptions to ACE, House, and Internship pages

--- src/pages/Ace.tsx
+++ src/pages/Ace.tsx
@@ -7,7 +7,7 @@ export function Ace() {
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <PageTitle title="ACE Program" />
         <h1 className="text-3xl font-bold text-white mb-6">ACE Program</h1>
-        <p className="text-gray-300">More details about the Anh Chi Em (ACE) program coming soon!</p>
+        <p className="text-gray-300">ACE stands for our Anh Chi Em program and it's VSA's big-little program! Each year, prospective "littos" meet and match up with their future bigs. Pairings can then develop into a close friendship, similar to that of actual siblings within a prior "fam". ACE usually consists of families with various and unique "lineages". It's like your actual family away from home, with bigs, "pseudos", siblings, and grands!</p>
       </div>
     </RevealOnScrollWrapper>
   );
--- src/pages/House.tsx
+++ src/pages/House.tsx
@@ -7,7 +7,7 @@ export function House() {
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <PageTitle title="House System" />
         <h1 className="text-3xl font-bold text-white mb-6">House System</h1>
-        <p className="text-gray-300">More details about the VSA House System coming soon!</p>
+        <p className="text-gray-300">The House system was created to provide members a smaller sub-group within VSA to easily meet new people, attend more close-knit events, and create memories with newfound friends. House can be compared to UCSD's colleges or the Hogwarts houses but unlike the fams established within the ACE program, the Houses are changed every year allowing members to consistently meet new people and experience different events every time. Each House is guided by two House Parents who look over all the members within the House, as well as plan, communicate, and execute several quarterly events for the House members to attend. There are also opportunities for Houses to "collab" and host events with each other in order to create bonds with other Houses as well. Each House is also able to earn points by participating in VSA events such as attending GBMS, external events, etc. in order to claim the grand prize at the end of the year. House is an amazing opportunity for members to create lasting friendships and comfortably meet new people through smaller events, and we hope you all come out and join us for a year of fun and new memories! </p>
       </div>
     </RevealOnScrollWrapper>
   );
