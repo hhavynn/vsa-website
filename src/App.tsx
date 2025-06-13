0f3b5a5492aaff286d3a0b02104141fccf7749c7
hhavynn <havynnguyen9@gmail.com>
2025-06-12 18:27:24 -0700

    Remove test component and red box

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
 import { NotFound } from './pages/NotFound';
-import { Test } from './components/Test';
 import { AdminEvents } from './pages/Admin/Events';
 import { Leaderboard } from './pages/Leaderboard';
 import { useAdmin } from './hooks/useAdmin';
@@ -25,26 +24,23 @@ function AdminRoute({ children }: { children: React.ReactNode }) {
 
 function App() {
   return (
-    <>
-      <Test />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="events" element={<Events />} />
-          <Route path="leaderboard" element={<Leaderboard />} />
-          <Route
-            path="admin/events"
-            element={
-              <AdminRoute>
-                <AdminEvents />
-              </AdminRoute>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="events" element={<Events />} />
+        <Route path="leaderboard" element={<Leaderboard />} />
+        <Route
+          path="admin/events"
+          element={
+            <AdminRoute>
+              <AdminEvents />
+            </AdminRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
