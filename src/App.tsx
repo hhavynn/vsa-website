28930f1f8831d1565b02132b16d3d13c93d71141
hhavynn <havynnguyen9@gmail.com>
2025-06-12 18:36:27 -0700

    Fix Router nesting issue by removing duplicate Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { Events } from './pages/Events';
@@ -11,21 +11,19 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <PointsProvider>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="events" element={<Events />} />
-              <Route path="leaderboard" element={<Leaderboard />} />
-              <Route path="admin/events" element={<AdminEvents />} />
-              <Route path="profile" element={<Profile />} />
-            </Route>
-          </Routes>
-        </PointsProvider>
-      </AuthProvider>
-    </Router>
+    <AuthProvider>
+      <PointsProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="events" element={<Events />} />
+            <Route path="leaderboard" element={<Leaderboard />} />
+            <Route path="admin/events" element={<AdminEvents />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </PointsProvider>
+    </AuthProvider>
   );
 }
 
